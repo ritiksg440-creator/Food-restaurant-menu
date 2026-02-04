ae70e045f471dc54ac73586859c8dd0133ecfbe7
Ritik Singh <ritiksg440@gmail.com>
2026-02-04 23:34:09 +0545

    update Food.js

--- Food.js
+++ Food.js
@@ -7,3 +7,17 @@ btns.forEach((btn)=>{
 setTimeout(() => btn.style.transform = "scale(1)", 100);
   })
 });
+const search=document.querySelector("#inp");
+const cards=document.querySelectorAll(".card");
+search.addEventListener("input",()=>{
+  const query=search.value.toLowerCase();
+  cards.forEach((card)=>{
+    const name=card.querySelector("h2").innerText.toLowerCase();
+    if(name.includes(query)){
+      card.style.display="block";
+    }
+    else{
+      card.style.display="none";
+    }
+  })
+})
