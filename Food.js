649599f8547931c1101d1089ca37e35ff2ba274c
Ritik Singh <ritiksg440@gmail.com>
2026-02-04 23:14:37 +0545

    update Food.js

--- Food.js
+++ Food.js
@@ -3,5 +3,7 @@ const btns=document.querySelectorAll(".card button");
 btns.forEach((btn)=>{
   btn.addEventListener("click",()=>{
     btn.innerText="Ordered";
+    btn.style.transform = "scale(0.95)";
+setTimeout(() => btn.style.transform = "scale(1)", 100);
   })
 });
